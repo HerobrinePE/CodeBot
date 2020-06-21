88f6380988063a6cac07c3dd5c6ac4d0bd616ae6
Glitch (full-married-jasper) <none>
2020-06-21 02:24:50 +0000

    ✈️📯 Updated with Glitch

--- bot.js
+++ bot.js
@@ -8,16 +8,8 @@ const router = require("./router.js");
 
 client.once('ready', () => {
     console.log('Ready!');
-    router.onLoad(client);//
 
-
-    client.user.setActivity("Political Hub🌍", { type: "STREAMING" });
-  
-    const server = client.guilds.cache.get(config.serverId);
-
-    config.messageData.forEach(data => handleRoles(server,data));
-
-
+    router.onLoad(client);
 
 });
 
